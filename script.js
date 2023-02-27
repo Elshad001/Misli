d30a83ca587b7e2526ab26a6efd5b0ffa626e5bc
Elshad001 <elshad.ismayilovv@mail.ru>
2023-02-27 16:42:17 +0400

    add section1 and section2

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ chevronLeft.addEventListener('click',()=>
 })
 
 
+let images=['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg'];
+let slider=document.querySelector('#slider');
+let duration=3000;
+let i=0;
+
+function imgslide()
+{
+    slider.style.backgroundImage=`url(${images[i]})`;
+
+    if(i<images.length-1)
+    {
+        i++
+    }
+    else
+    {
+        i=0
+    }
+    setTimeout('imgslide()',duration);
+}
+window.onload=imgslide;
+
+let daxilOl=document.querySelector("#daxilol");
+let backIcon=document.querySelector('#backicon');
+let loginBtn1=document.querySelector('#login-btn1')
+let loginBtn2=document.querySelector('#login-btn2');
